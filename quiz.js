d13ba2e83dfa0dd08189a191c8764f472146deae
JoyMumen <joymumengraphic@gmail.com>
2025-01-18 12:03:34 +0100

    update

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+  const correctAnswer = "4";
+  let input = document.querySelector("[name=quiz]:checked");
+  let userAnswer = input.value;
+  let result = document.getElementById("feedback");
+
+  if (userAnswer === correctAnswer) {
+    result.textContent = "Correct! Well done.";
+  } else {
+    result.textContent = "That's incorrect. Try again!";
+  }
+}
+
+let submitButton = document.querySelector("#submit-answer");
+
+submitButton.addEventListener("click", checkAnswer);
